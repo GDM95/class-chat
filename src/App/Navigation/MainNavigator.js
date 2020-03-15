92966256718e15182886338444ee4f598d2e7883
Greg Melillo <gregmelillo@Gregs-MacBook-Pro-490.local>
2020-03-15 18:27:45 -0400

    update README

--- src/App/Navigation/MainNavigator.js
+++
@@ -1,8 +0,0 @@
-import { createSwitchNavigator } from 'react-navigation';
-
-//import Main from '../Main'
-import DrawerNavigator from '../Navigation/DrawerNavigator';
-
-export default createSwitchNavigator({
-  DrawerNavigator
-});
--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const PRIMARY_COLOR = "#CC0033"
-export const SECONDARY_COLOR = "#0099cc"
-
-export const PRIMARY_FONT = 'Apple SD Gothic Neo'
-export const SECONDARY_FONT = 'AppleSDGothicNeo-SemiBold' // AppleSDGothicNeo-Regular
